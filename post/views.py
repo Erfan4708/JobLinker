18785a9d28f1331a142f0583b834fadfe7649ed8
Erfan Abbasi <abbasy.4708@gmail.com>
2023-08-13 10:21:39 +0330

    edit templates and add search form

--- post/views.py
+++ post/views.py
@@ -7,6 +7,8 @@ from celery import shared_task
 from .tasks import jobinja_scrap, jobvision_scrap
 from .models import Post
 from django.views import generic
+from django.db.models import Q
+
 
 
 class PostListView(generic.ListView):
@@ -15,9 +17,24 @@ class PostListView(generic.ListView):
     model = Post
     paginate_by = 20
     template_name = 'post_list.html'
+    ordering = ['date_modified']
     context_object_name = 'posts'
 
 
 class PostDetailView(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+def search(request):
+    search_result = None
+    if request.method == "POST":
+        search_keyword = request.POST.get("search_keyword")
+        q_condition = Q(title__icontains=search_keyword) | \
+                      Q(company_name__icontains=search_keyword) | \
+                      Q(detail_position__icontains=search_keyword) | \
+                      Q(description_position__icontains=search_keyword) | \
+                      Q(location__icontains=search_keyword)
+
+        search_result = Post.objects.filter(q_condition)
+    return render(request, 'search_result.html', {'search_result': search_result})
